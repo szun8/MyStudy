82a526164df4c0668766c1b7f968904a7649dff8
최시은 <chse0810@gmail.com>
2022-12-30 02:08:35 +0900

    무한 스크롤링 배경구현
    3D object Quad사용

---
+++ Unity/InfinityBG/Assets/BGScroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    private MeshRenderer render;
+
+    public float speed;
+    private float offset;
+
+    void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += Time.deltaTime * speed;
+        render.material.mainTextureOffset = new Vector2(offset , 0);
+    }
+}
